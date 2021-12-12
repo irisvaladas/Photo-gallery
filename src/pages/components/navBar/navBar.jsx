9376a602d7e3216650f8150d03acf4f8ceb04e04
irisvaladas <irismprlval@hotmail.com>
2021-12-12 17:57:07 +0000

    adding routes and connection between pages

---
+++ src/pages/components/navBar/navBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../../files/logo.png';
+
+const navBar = () => {
+    <div className="navBar">
+        <h1>hello world</h1>
+        
+        <div className='logo'>
+            <img src={logo} alt='Logo'/>
+            
+        </div>
+    </div>
+}
+
+export default navBar;
---
+++ src/pages/guest/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import navBar from "../components/navBar/navBar.jsx";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <navBar />
+    </div>
+  );
+};
+
+export default HomePage;
