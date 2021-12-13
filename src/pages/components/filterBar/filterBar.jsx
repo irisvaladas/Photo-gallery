8ae127ca933df0956f64497607823b0243295684
irisvaladas <irismprlval@hotmail.com>
2021-12-13 15:18:42 +0000

    created filterbar with css

---
+++ src/pages/components/filterBar/filterBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './filterBar.css'
+
+const FilterBar = () => {
+    return (
+        <div className='filterBar'>
+            <button>City</button>
+            <button>Nature</button>
+            <button>Country site</button>
+            <button>Beach</button>
+            <button>Snow</button>
+            <button>Japan</button>
+        </div>
+    )
+}
+
+export default FilterBar;
--- src/pages/guest/HomePage.jsx
+++ src/pages/guest/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../components/navBar/navBar.jsx";
 import SlideDisplay from "../components/slideDisplay/slideDisplay.jsx";
+import FilterBar from  "../components/filterBar/filterBar.jsx";
 
 export const HomePage = () => {
   return (
     <div>
       <NavBar />
       <SlideDisplay />
+      <FilterBar />
     </div>
   );
 };
