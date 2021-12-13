be84496aa3a45e0d23d5757fcb88bb654230be3f
irisvaladas <irismprlval@hotmail.com>
2021-12-13 12:51:46 +0000

    take default padding and margin from the root

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage, RegistrationPage, LoginPage } from "./pages";
 
+
 function App() {
   return (
     <Router>
